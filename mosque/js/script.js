0948a70e9c943c20dee2ce124ae4b9eaefd59bd3
Tuhin <60627930+tth18th@users.noreply.github.com>
2025-03-20 01:49:29 +0000

    Delete mosque directory

--- mosque/js/script.js
+++
@@ -1,5 +0,0 @@
-// Custom JavaScript for the Mosque website
-document.addEventListener("DOMContentLoaded", function() {
-    // You can add custom interactive scripts here
-    console.log("Mosque website loaded");
-});
